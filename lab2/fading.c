4c2972d07831be9b1b0c5854e72b67d6b4a7b15e
riking <rikingcoding@gmail.com>
2013-10-04 08:46:50 -0700

    First attempt; does not work due to delay.h function
    The _delay_us() method is expecting a compile-time integer constant, which I am not providing here.

--- lab2/fading.c
+++ lab2/fading.c
@@ -11,6 +11,38 @@
 
 #include "masks.h"
 
-int main(void) {
+#define LED1 PD3
+#define LED2 PD5
+#define LED3 PD7
 
+#define PERIOD_us 1000
+
+void doCycle(int pin, float duty) {
+	int onTime = (int) (PERIOD_us * duty);
+	int offTime = PERIOD_us - onTime;
+	ONPIN(PORTD, pin);
+	_delay_us(onTime);
+	OFFPIN(PORTD, pin);
+	_delay_us(offTime);
 }
+
+void fadeInOut(int pin) {
+	float duty;
+	for (duty = 0; duty < 1; duty += 0.2) {
+		doCycle(pin, duty);
+		doCycle(pin, duty);
+		doCycle(pin, duty);
+	}
+	for (duty = 1; duty > 0; duty += 0.2) {
+		doCycle(pin, duty);
+		doCycle(pin, duty);
+		doCycle(pin, duty);
+	}
+}
+
+int main(void) {
+	fadeInOut(LED1);
+	fadeInOut(LED2);
+	fadeInOut(LED3);
+}
+
