28a36fc2ef7a074eddc7d2881e47f73f975da275
riking <rikingcoding@gmail.com>
2013-10-07 22:06:20 -0700

    Write part1

--- lab2/lab2_part1.c
+++ lab2/lab2_part1.c
@@ -11,14 +11,27 @@
 
 #include "masks.h"
 
-#define LED1 PD3
+#define LED1 PD4
 #define SW1 PD6
 
+// 
+void blinkLed() {
+	ONPIN(PORTD, LED1);
+	_delay_ms(120);
+	OFFPIN(PORTD, LED1);
+	_delay_ms(280);
+}
+
 int main(void) {
+	// setup
 	ONPIN(DDRD, LED1);
 	OFFPIN(DDRD, SW1);
+
 	while (1) {
-		...
+		// delay 'while' the switch pin is high
+		while (GETPIN(PIND, SW1))
+			_delay_ms(100);
+		blinkLed(); blinkLed();blinkLed();blinkLed();blinkLed();
 	}
 }
 
