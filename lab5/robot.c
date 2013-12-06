cbb62188fabba7c4171a4c6827faacfa89b0791e
Nicolas Avila <serkestral@gmail.com>
2013-12-05 19:16:02 -0800

    Update robot.c

--- lab5/robot.c
+++ lab5/robot.c
@@ -52,11 +52,11 @@ char falling_edge(int timeout, char hf) {
 }
 
 char check_starting_bit(char hf){
-	char falling_edgeA = falling_edge(150, hf);
+	char falling_edgeA = falling_edge(62.5, hf);
 		if (falling_edgeA == 0){
 			return 0;
 		} else {
-			char falling_edgeB = falling_edge(155, hf);
+			char falling_edgeB = falling_edge(156.25, hf);
 			if (falling_edgeB == 1) {
 				return 0;
 			} else {
